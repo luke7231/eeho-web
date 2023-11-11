309e49c1ca157785ed6d045b8bb1fd987012a150
luke <luke@lukes-MacBook-Air.local>
2023-11-10 20:04:15 -0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  function postMessage() {
+    window.ReactNativeWebView.postMessage("안녕하세요");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <button onClick={postMessage}>click Me!</button>    
     </div>
   );
 }
