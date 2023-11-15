7413484b2aea29e7a270383c955de367b009c545
luke <luke@lukes-MacBook-Air.local>
2023-11-14 20:50:10 -0800

    feat: screen 최대 가로폭 지정 & 가운데 정렬

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import GlobalStyle from "./global-style";
 import Router from "./router";
+import styled from "styled-components";
+import { getResponsiveMaxWidth } from "./utils/layout-util";
+
+const Wrapper = styled.section`
+    height: 100%;
+    width: 100%;
+    margin: 0 auto !important;
+    ${getResponsiveMaxWidth()}
+`;
 
 function App() {
     // function postMessage() {
@@ -10,7 +19,9 @@ function App() {
     return (
         <>
             <GlobalStyle />
-            <Router />
+            <Wrapper>
+                <Router />
+            </Wrapper>
         </>
     );
 }
--- src/Routes/home/index.tsx
+++ src/Routes/home/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             THIS IS HOME
             <br />
-            <Link to="/onboarding/:id">onboarding</Link>
+            <Link to="/onboarding">onboarding</Link>
         </div>
     );
 };
