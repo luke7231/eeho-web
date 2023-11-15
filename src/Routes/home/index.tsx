7290ae652e157c402a7b997f70f8b08d602339ec
luke <luke@lukes-MacBook-Air.local>
2023-11-15 12:45:14 -0800

    feat: agree 페이지 밑단 완성

--- src/Routes/home/index.tsx
+++ src/Routes/home/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+`;
 
 const Home = () => {
     return (
-        <div>
+        <Container>
             THIS IS HOME
             <br />
             <Link to="/onboarding">onboarding</Link>
-        </div>
+        </Container>
     );
 };
 export default Home;
--- src/Routes/onboarding/agree.tsx
+++ src/Routes/onboarding/agree.tsx
@@ -1,5 +1,77 @@
+import styled from "styled-components";
+import HLogo from "../../components/logo/h-logo";
+import VMark from "../../images/icons/v_mark.png";
+import VMarkGreen from "../../images/icons/v_mark_green.png";
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
+const TitleSection = styled.div`
+    margin-left: 35px;
+    margin-top: 45px;
+`;
+const Title = styled.div`
+    color: #462d2d;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 800;
+    margin-top: 8px;
+`;
+const AgreeContainer = styled.div`
+    margin-top: 38px;
+    margin-left: 48px;
+`;
+const CheckWrap = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const CheckBoxContainer = styled.div<{ checked: boolean }>`
+    width: 30px;
+    height: 30px;
+    background: ${(props) => (props.checked ? "#A7B387" : "#fff")};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 13px;
+`;
+const CheckIcon = styled.img`
+    width: 19px;
+    height: 12px;
+`;
+const CheckBoxText = styled.div`
+    color: #462d2d;
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 700;
+`;
+const ButtonContainer = styled.div``;
+
+const CheckBox = ({ checked, text }: { checked: boolean; text: string }) => {
+    return (
+        <CheckWrap>
+            <CheckBoxContainer checked={checked}>
+                <CheckIcon src={checked ? VMark : VMarkGreen} />
+            </CheckBoxContainer>
+            <CheckBoxText>{text}</CheckBoxText>
+        </CheckWrap>
+    );
+};
 const Agree = () => {
-    return <div>Agree</div>;
+    return (
+        <Container>
+            <TitleSection>
+                <HLogo color="brown" width={43} height={88} />
+                <Title>에호 약관동의</Title>
+            </TitleSection>
+            <AgreeContainer>
+                <CheckBox checked={true} text="약관동의1" />
+                <CheckBox checked={false} text="약관동의2" />
+            </AgreeContainer>
+            <ButtonContainer></ButtonContainer>
+        </Container>
+    );
 };
 
 export default Agree;
