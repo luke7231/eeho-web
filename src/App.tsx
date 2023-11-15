da2ae42a817b4104c0ee8a4a844aa68dc9912c64
luke <luke@lukes-MacBook-Air.local>
2023-11-14 20:00:45 -0800

    feat: eslint, prettier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  function postMessage() {
-    window.ReactNativeWebView.postMessage("안녕하세요");
-  }
-  return (
-    <div style={{width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <button onClick={postMessage}>click Me!</button>    
-    </div>
-  );
+    function postMessage() {
+        window.ReactNativeWebView.postMessage("안녕하세요");
+    }
+    return (
+        <div
+            style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}
+        >
+            <button onClick={postMessage}>click Me!</button>
+        </div>
+    );
 }
 
 export default App;
