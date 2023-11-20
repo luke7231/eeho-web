512a16820dbe2f311f24f349b17dd2dfd2f2c7e9
luke <luke@lukes-MacBook-Air.local>
2023-11-20 11:06:20 -0800

    feat: setting 페이지 예시 데이터 추가

--- src/Routes/settings/index.tsx
+++ src/Routes/settings/index.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+interface Photo {
+    img: string;
+}
 const Settings = () => {
-    return <div>Settings</div>;
+    const [list, setList] = useState<Photo[] | undefined>();
+    useEffect(() => {
+        fetch(process.env.REACT_APP_SERVER_URI + "/album/image/index", {
+            headers: {
+                token: localStorage.getItem("jwt") as string,
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => setList(data.photos));
+    }, []);
+    console.log(list);
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            {list?.map((photo) => <img src={photo.img} style={{ height: 300, width: 300, objectFit: "cover" }} />)}
+        </div>
+    );
 };
 
 export default Settings;
