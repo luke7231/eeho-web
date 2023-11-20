2e2ba77833e7a1bc3b1e52f4c251315ab41fab2f
luke <luke@lukes-MacBook-Air.local>
2023-11-20 13:28:48 -0800

    feat: react-copy-to-clipboard libarary install & 결과 페이지 완성

--- src/Routes/onboarding/sign-up/create-family/creation-result.tsx
+++ src/Routes/onboarding/sign-up/create-family/creation-result.tsx
@@ -1,7 +1,69 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../../../contexts/auth-provider";
 import Button from "../../../../components/onboarding/button";
+import styled from "styled-components";
+import EehoLogo from "../../../../images/icons/EEHO.png";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
+const Contanier = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 0 48px;
+`;
+const Logo = styled.img`
+    width: 95px;
+    height: 61px;
+    margin-bottom: 24px;
+`;
+const Title = styled.div`
+    height: 70px;
+    display: flex;
+    align-items: center;
+    font-size: 24px;
+    font-weight: 800;
+    line-height: 29px;
+    letter-spacing: 0em;
+    text-align: center;
+`;
+const Text = styled.div`
+    height: 75px;
+    display: flex;
+    align-items: center;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 19px;
+    letter-spacing: 0em;
+    text-align: center;
+    margin-bottom: 20px;
+`;
+const CopyTextBox = styled.div`
+    background-color: #bcd6ab;
+    border: 0;
+    width: 100%;
+    text-align: center;
+    font-size: 24px;
+    margin-bottom: 48px;
+    border-radius: 10px;
+    padding: 6px 0;
+`;
+const Margin = styled.div`
+    margin-bottom: 40px;
+`;
+const CheckButton = styled.button`
+    width: 95px;
+    height: 44px;
+    background-color: #627d50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    border-radius: 10px;
+`;
 const CreationResult = () => {
     const { login } = useAuth();
     const navigate = useNavigate();
@@ -14,11 +76,24 @@ const CreationResult = () => {
         login();
         navigate("/");
     };
+    const copyText = `www.google.com \n\n 코드: ${familyCode}`;
     return (
-        <>
-            <div style={{ margin: 100 }}>가족 code: {familyCode}</div>
+        <Contanier>
+            <Logo src={EehoLogo} />
+            <Title>가족 초대 링크</Title>
+            <Text>
+                함께 에호하고싶은
+                <br /> 가족 구성원에게 공유해보세요.
+            </Text>
+            <CopyTextBox>
+                www.google.com <Margin /> 코드: {familyCode}
+            </CopyTextBox>
+            <CopyToClipboard text={copyText} onCopy={() => alert("복사가 완료되었습니다!")}>
+                <CheckButton>복사</CheckButton>
+            </CopyToClipboard>
+
             <Button text="에호 시작하기" onClick={onClickButton} />
-        </>
+        </Contanier>
     );
 };
 
